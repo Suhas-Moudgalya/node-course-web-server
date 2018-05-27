025e7003dab9a8c4283c2a34c29f13a31006dab3
Suhas Moudgalya <suhas.moudgalya@ad.infosys.com>
2018-05-27 11:54:13 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req,res)=>{
+    // res.send({
+    //     name: 'SMS',
+    //     likes: ['Cricket', 'Chess', 'Mindgames']
+    // });
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to express-app home page.'
+    });
+});
+
+app.get('/about', (req,res)=>{
+res.render('about.hbs',{
+    pageTitle: 'About Page'
+});
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
